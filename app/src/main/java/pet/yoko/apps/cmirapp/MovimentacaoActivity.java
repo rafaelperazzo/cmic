0b2cd286ca601ee32481fe8e92cf1ca2ec30d80c
Rafael Perzzzo <rafaelperazzo@gmail.com>
2020-07-20 15:18:25 -0300

    Atualizado 2020-07-20 15:18:25

--- app/src/main/java/pet/yoko/apps/cmirapp/MovimentacaoActivity.java
+++ app/src/main/java/pet/yoko/apps/cmirapp/MovimentacaoActivity.java
@@ -2,8 +2,13 @@ package pet.yoko.apps.cmirapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
@@ -12,11 +17,18 @@ import java.util.concurrent.ExecutionException;
 
 import pet.yoko.apps.cmirapp.db.DatabaseClient;
 import pet.yoko.apps.cmirapp.db.Item;
+import pet.yoko.apps.cmirapp.tasks.TaskCadastrarMovimentacao;
 import pet.yoko.apps.cmirapp.tasks.TaskCarregarItems;
 
 public class MovimentacaoActivity extends AppCompatActivity {
     SearchableSpinner cmbItem;
+    Spinner cmbSetor;
+    Spinner cmbFinalidade;
+    EditText txtQuantidade;
+    EditText txtDetalhes;
+    ProgressBar progresso;
     List<Item> items;
+    TextView txtStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,13 @@ public class MovimentacaoActivity extends AppCompatActivity {
         cmbItem = (SearchableSpinner) findViewById(R.id.cmbItem);
         cmbItem.setTitle("Escolha o item");
         cmbItem.setPositiveButton("OK");
+        cmbSetor = (Spinner)findViewById(R.id.cmbSetor);
+        cmbFinalidade = (Spinner)findViewById(R.id.cmbFinalidade);
+        txtQuantidade = (EditText) findViewById(R.id.txtQuantidade);
+        txtDetalhes = (EditText) findViewById(R.id.txtDetalhes);
+        progresso = (ProgressBar)findViewById(R.id.progressoMovimentacao);
+        progresso.setVisibility(View.GONE);
+        txtStatus = (TextView)findViewById(R.id.txtStatus);
         TaskCarregarItems tci = new TaskCarregarItems(DatabaseClient.getInstance(getApplicationContext()).getAppDatabase(),cmbItem,getApplicationContext());
         try {
             items = tci.execute().get();
@@ -32,6 +51,55 @@ public class MovimentacaoActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void cadastrarClick(View v) {
+        if (isNotEmpty(txtQuantidade) && isNotEmpty(txtDetalhes)) {
+            int item = cmbItem.getSelectedItemPosition();
+            int item_id = items.get(item).getId();
+            float quantidade = 0;
+            try {
+                quantidade = Float.parseFloat(txtQuantidade.getText().toString());
+            }
+            catch (NumberFormatException e) {
+                quantidade = 0;
+            }
+
+            String setor = cmbSetor.getSelectedItem().toString();
+            String finalidade = cmbFinalidade.getSelectedItem().toString();
+            String detalhes = txtDetalhes.getText().toString();
+            TaskCadastrarMovimentacao submit = new TaskCadastrarMovimentacao(DatabaseClient.getInstance(getApplicationContext()).getAppDatabase(),progresso,getApplicationContext(),item_id,quantidade,setor,finalidade,detalhes);
+            String resposta = "";
+            try {
+                resposta = submit.execute().get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                resposta = "ExecutionException";
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                resposta = "InterruptedException";
+            }
+            if (resposta.equals("200")) {
+                txtStatus.setText("Movimentação cadastrada com sucesso");
+                txtStatus.setBackgroundColor(Color.GREEN);
+                txtStatus.setTextColor(Color.BLACK);
+            }
+            else {
+                txtStatus.setText(resposta);
+                txtStatus.setBackgroundColor(Color.RED);
+                txtStatus.setTextColor(Color.WHITE);
+            }
+        }
+    }
+
+    private boolean isNotEmpty(EditText edit) {
+        if (edit.getText().toString().equals("")) {
+            edit.setError("Precisa ser preenchido!");
+            return (false);
+        }
+        else {
+            return (true);
         }
     }
 }
---
+++ app/src/main/java/pet/yoko/apps/cmirapp/tasks/TaskCadastrarMovimentacao.java
@@ -0,0 +1,91 @@
+package pet.yoko.apps.cmirapp.tasks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import pet.yoko.apps.cmirapp.Ferramenta;
+import pet.yoko.apps.cmirapp.db.AppDatabase;
+
+public class TaskCadastrarMovimentacao extends AsyncTask<Void,Void,String> {
+
+    AppDatabase db;
+    ProgressBar progresso;
+    Context context;
+    int item;
+    float quantidade;
+    String setor;
+    String finalidade;
+    String detalhes;
+    OkHttpClient client = new OkHttpClient();
+
+    public TaskCadastrarMovimentacao(AppDatabase db, ProgressBar progresso, Context context, int item, float quantidade, String setor, String finalidade, String detalhes) {
+        this.db = db;
+        this.progresso = progresso;
+        this.context = context;
+        this.item = item;
+        this.quantidade = quantidade;
+        this.setor = setor;
+        this.finalidade = finalidade;
+        this.detalhes = detalhes;
+    }
+
+    private String run() {
+        RequestBody formBody = new FormBody.Builder()
+                .add("item", String.valueOf(this.item))
+                .add("quantidade", String.valueOf(this.quantidade))
+                .add("finalidade", this.finalidade)
+                .add("descricao", this.detalhes)
+                .add("ua", this.setor)
+                .add("token", Ferramenta.getPref("token","null"))
+                .build();
+        Request request = new Request.Builder()
+                .url("https://sci02-ter-jne.ufca.edu.br/cgs/processar_movimentacao.php")
+                .post(formBody)
+                .build();
+
+        Call call = client.newCall(request);
+        String resposta = "";
+        try {
+            Response response = call.execute();
+            resposta= response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            resposta = "ERRO: Informar ao desenvolvedor!";
+        }
+        return (resposta.trim());
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progresso.setVisibility(View.VISIBLE);
+        progresso.getLayoutParams().height = LinearLayout.LayoutParams.MATCH_PARENT;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        String resposta = run();
+        return (resposta);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        progresso.setVisibility(View.GONE);
+        progresso.getLayoutParams().height = LinearLayout.LayoutParams.WRAP_CONTENT;
+    }
+}
